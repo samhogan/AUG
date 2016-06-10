f2b5299c28d51f05e32829ce27738dacfe2f31fc
samhogan <sam@samhogan.net>
2016-06-10 12:55:14 -0400

    new atlas and rgb colors rather than points

--- Assets/Scripts/Substances.cs
+++ Assets/Scripts/Substances.cs
@@ -19,12 +19,23 @@ public class Substance
 		
 	}
 
-	public Substance(float xcp, float ycp, double sa)
+
+	public Substance(int x, int y, double sa) : this(0, 0, 153, sa)
 	{
+	}
+
+	public Substance(int r, int g, int b, double sa)
+	{
+		//calculate the color position in the atlas
+		int bpos = Mathf.CeilToInt(b/17f);
+		float xcp = (bpos%4)*16+Mathf.CeilToInt(g/17f);
+		float ycp = (bpos/4)*16+Mathf.CeilToInt(r/17f);
+			
+
 		xcp /= atlasWidth;//texture points are in the range [0,1]
 		ycp /= atlasWidth;
 		colorPoint = new Vector2(xcp, ycp);
-
+		//Debug.Log(xcp+" "+ycp);
 		surfAb = sa;
 	}
 
@@ -57,23 +68,23 @@ public class Substance
 	//a dictionary of an instances of each substance
 	public static Dictionary<Sub, Substance> subs = new Dictionary<Sub, Substance>() 
 	{
-		{Sub.TEST, new Substance(16,16,0)},
-		{Sub.WaterIce, new Substance(31,0,1)},
-		{Sub.Vegitation1, new Substance(9,23,1)},
-		{Sub.Vegitation2, new Substance(4,19,1)},
-		{Sub.Vegitation3, new Substance(8,26,1)},
-		{Sub.Sand, new Substance(15, 29, 20)},
-		{Sub.Dirt, new Substance(5, 29, 1)},
-		{Sub.Mud, new Substance(2, 30,1 )},
-		{Sub.Basalt, new Substance(6,0, 34)},
-		{Sub.Baslat2, new Substance(8,0, 33)},
-		{Sub.Baslat3, new Substance(10,0, 33)},
-		{Sub.IronDioxide, new Substance(10,30,10)}, 
-		{Sub.IronDioxide2, new Substance(10,31, 0)}, 
-		{Sub.Gold, new Substance(18,27,0.01)},
-		{Sub.Granite, new Substance(27,31,33)},//igneous
-		{Sub.Andesite, new Substance(15,0,33)},//igneous
-		//{Sub.Anorthosite, new Substance(15,0,33)},//igneous
+		{Sub.TEST, new Substance(2,254,255,0)},
+		{Sub.WaterIce, new Substance(255,255,255,1)},
+		{Sub.Vegitation1, new Substance(47,155,0,1)},
+		{Sub.Vegitation2, new Substance(0,84,37,1)},
+		{Sub.Vegitation3, new Substance(123,141,0,1)},
+		{Sub.Sand, new Substance(240,135,0, 20)},
+		{Sub.Dirt, new Substance(98,55,0, 1)},
+		{Sub.Mud, new Substance(56,21,0,1 )},
+		{Sub.Basalt, new Substance(45,45,45, 34)},
+		{Sub.Baslat2, new Substance(61,61,61, 33)},
+		{Sub.Baslat3, new Substance(78,78,78, 33)},
+		{Sub.IronDioxide, new Substance(169,63,0,10)}, 
+		{Sub.IronDioxide2, new Substance(169,31,0, 0)}, 
+		{Sub.Gold, new Substance(255,242,29,0.01)},
+		{Sub.Granite, new Substance(217,163,132,33)},//igneous
+		{Sub.Andesite, new Substance(180,183,183,33)},//igneous
+		{Sub.Anorthosite, new Substance(172,162,140,33)},//igneous
 	};
 
 
