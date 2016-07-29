8c678fed91b896f4959338c269ce7c76e982f10b
samhogan <sam@samhogan.net>
2016-07-28 22:14:41 -0400

    grab item rotation hack

---
+++ Assets/Scripts/DebugScripts/RotTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotTest : MonoBehaviour {
+
+    public GameObject other;
+
+	// Use this for initialization
+	void Start ()
+    {
+        print(other.transform.localRotation.eulerAngles);
+        other.transform.parent = transform;
+        print(other.transform.localRotation.eulerAngles);
+        transform.rotation = Quaternion.Euler(0, 60, 0);
+        print(other.transform.localRotation.eulerAngles);
+        other.transform.parent = null;
+        print(other.transform.localRotation.eulerAngles);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
--- Assets/Scripts/PlayerGrab.cs
+++ Assets/Scripts/PlayerGrab.cs
@@ -7,11 +7,16 @@ public class PlayerGrab : MonoBehaviour
     private GameObject curObject;
     private Rigidbody curBody;
 
+    //the rotation of the camera in the previous fixed update
+    //private Vector3 lastRot;
 
-	// Use this for initialization
-	void Start ()
+    //the rotation of the curObject at pickup relative to the camera
+    private Quaternion targetRot;
+
+    // Use this for initialization
+    void Start ()
     {
-	
+        //lastRot = transform.rotation.eulerAngles;
 	}
 	
 	// Update is called once per frame
@@ -27,6 +32,22 @@ public class PlayerGrab : MonoBehaviour
                 curBody.useGravity = false;
                 curObject = hitInfo.rigidbody.gameObject;
 
+                //targetRot = Quaternion.Euler(transform.rotation.eulerAngles - curObject.transform.rotation.eulerAngles);//curBody.rotation * Quaternion.Inverse(transform.rotation);
+                //WHAT A HACK!!
+                //I am completely confused why my attempted mathematical implementation of this failed
+                //TODO: fix this, although not top priority
+                //my guess is that it will never get fixed ever, today's date is 7/28/16
+                curObject.transform.parent = transform;
+                targetRot = curObject.transform.localRotation;
+                curObject.transform.parent = null;
+
+
+
+
+            }
+            else
+            {
+                dropItem();
             }
         }
 	}
@@ -39,6 +60,24 @@ public class PlayerGrab : MonoBehaviour
             Vector3 destination = transform.position + transform.forward * 2;
             //curBody.AddForce((destination - curBody.position)*5, ForceMode.Force);
             curBody.velocity = (destination - curBody.position) * 10;
+
+            //this does not work very well
+            //TODO: fix it
+            //curBody.angularVelocity = transform.rotation.eulerAngles - lastRot;
+            // curBody.angularVelocity = Vector3.zero;
+
+            //add the camera rot and target rot
+            curBody.rotation = transform.rotation * targetRot;
+            //Vector3 destRot = 
         }
+        //lastRot = transform.rotation.eulerAngles;
+    }
+
+    //drops the current item
+    void dropItem()
+    {
+        curBody.useGravity = true;
+        curBody = null;
+        curObject = null;
     }
 }
